252660eb110ed52ba10aa2e7fa06a495f700d9d7
Stefan von Stein <stefan@sandra.(none)>
2010-09-01 09:41:05 +0200

    newdata i fint format

--- server/src/se/sj/monitor/newdata.clj
+++ server/src/se/sj/monitor/newdata.clj
@@ -3,93 +3,113 @@
   (:import [java.io ByteArrayOutputStream DataOutputStream])
   (:import [java.text SimpleDateFormat])
   (:import [java.util Date])
-  (:use [clojure test])
-  (:use cupboard.bdb.je)
-  (:use cupboard.utils)
+  (:use [clojure.test :only [is deftest]])
+  (:use [cupboard.bdb.je :only [db-put db-get db-open db-close]])
+  (:use [cupboard.utils :only [make-temp-dir rmdir-recursive]] )
+  (:use [se.sj.monitor.db :only [using-db *db-env*]]))
 
-  (:use se.sj.monitor.db))
+;(set! *warn-on-reflection* true)
 
-(set! *warn-on-reflection* true)
-
-(def index-by-name (ref {}))
-(def name-by-index (ref {}))
-(def next-index (ref 0))
-(def current-day (ref nil))
 (def dayindex-db (atom nil))
-(def current-days (atom nil))
+; A set of day-structures by date
+(def current-days (atom {}))
+(def lru (atom []))
 
-(defn save-index []
+(defn put-last [s e]
+     (lazy-cat (remove #(= e %) s) [e]))
+
+(defn save-index [days day]
   (when @dayindex-db
-  (db-put @dayindex-db @current-day @name-by-index)))
+    (when-let [current (get days day)]
+      (db-put @dayindex-db day (:by-index current)))))
 
-(defn load-day [day]
-;(println (str "loading " day ". Current " @current-day))
-(when @dayindex-db
-  (if-let [the-day (second (db-get @dayindex-db day))]
-    (let [by-name (reduce #(assoc %1 (val %2) (key %2)) {} the-day)]
-;      (println (str "Loaded " day ", containing " the-day)) 
-      (dosync 
-       (ref-set name-by-index  the-day)
-       (ref-set index-by-name by-name)
-       (ref-set current-day day)
-       (ref-set next-index (count the-day))
-     ))
-    (do
- ;     (println (str "Made new " day))
-    (dosync 
-     (ref-set name-by-index {})
-     (ref-set index-by-name {})
-     (ref-set current-day day)
-     (ref-set next-index 0)
-    )))))
+(defn load-day 
+  "Returns a day structure from database, or an empty if no db is used, or no data was found for the day"
+  [day]
+  (let [empty (assoc {} :by-index {} :by-name {} :next-index 0)]
+    (if @dayindex-db
+      (if-let [by-index (second (db-get @dayindex-db day))]
+	(do 
+;	  (println "Found day")
+	  (let [by-name (reduce #(assoc %1 (val %2) (key %2)) {} by-index)]
+	    (assoc {} :by-index by-index :by-name by-name :next-index (count by-index))))
+	(do ;(println "Created day") 
+	    empty))
+      empty)))
+
+
+  
+(defn get-day [day]
+  ;(println "Get day")
+  (if-let [the-day (get @current-days day)]
+    the-day
+    (let [the-day (load-day day)]
+      ;(println "loaded days")
+      	(swap! current-days #(assoc % day the-day))
+	(swap! lru (fn [d] (let [r (put-last d day)]
+			     (if (< 10 (count r))
+			       (next r)
+			       r))))
+	;remove all not found in lru
+	(swap! current-days (fn [c] 
+			      (select-keys c @lru)))
+	the-day)))
+
 
 (defmacro using-dayindex-db
   [db-env name & form]
   `(when-let [dbii# (db-open ~db-env ~name :allow-create true)]
-     (swap! dayindex-db (fn [c#] dbii#))
-     (dosync 
-      (ref-set index-by-name {})
-      (ref-set name-by-index {})
-      (ref-set current-day nil)
-      (ref-set next-index 0))
+     (reset! dayindex-db dbii#)
+     (reset! current-days {})
      (try
       (do ~@form)
       (finally
-       (dosync 
-	(ref-set index-by-name {})
-	(ref-set name-by-index {})
-	(ref-set current-day nil)
-	(ref-set next-index 0))
-       (swap! dayindex-db (fn [_#] nil ))
+       (reset! current-days  {})
+       (reset! dayindex-db nil)
        (db-close dbii#)))))
 
-(defn index-for-name [day name]
-  (when (not (= @current-day day))
-    (load-day day))
-  (if-let [index (get @index-by-name name)]
-    index
-    (do
-      (let [result (atom nil)]
-	(dosync
-	 (alter index-by-name assoc name @next-index)
-	 (alter name-by-index assoc @next-index name)
-	 (alter next-index inc)
-	 (swap! result (fn [l] (dec @next-index))))
-	(save-index)
-	@result))))
-    
 
+(defn index-for-name [day name]
+  (let [the-day (get-day day)]
+    (if-let [index (get (:by-name the-day) name)]
+      (do ;(println "Found it")
+	  index)
+      (do
+	(let [new-days 
+	      (swap! current-days 
+		     (fn [cd] 
+		       (if-let [the-day (get cd day)]
+			 (if-let [index (get (:by-name the-day) name)]
+			   (do ;(println "Found, added just prior to transaction") 
+			       cd)
+			   (do ;(println "Created new") 
+			       (let [index (:next-index the-day)
+				     next-index (inc index)
+				     by-name (assoc (:by-name the-day) name index)
+				     by-index (assoc (:by-index the-day) index name)]
+				 (assoc cd day {:by-name by-name :by-index by-index :next-index next-index}))))
+			 (do ;(println "This day was removed just prior to the transaction")
+			     (assoc cd day {:by-name {name 0} :by-index {0 name} :next-index 1})))))]
+	;(println (str "Save " day))
+	(save-index new-days day)
+	;(println (str "New:" new-days " Name:" name))
+	;(let [the-day (get new-days day)]
+	 ; (println the-day))
+	(get (:by-name (get new-days day)) name))))))
+				  
+
+      
 
 (defn name-for-index [day index]
-  (when (not (= @current-day day))
-    (load-day day))
-  (get @name-by-index index))
+  (get (:by-index (get-day day)) index))
 
 (def *date-formatter* nil)
 
 (defn new-date-formatter []
   (SimpleDateFormat. "yyyyMMdd"))
 
+
+;  ********************************************HERE******** also, add lru removal
 (defn to-bytes [data]
   (let [array-stream (ByteArrayOutputStream.)
 	data-out (DataOutputStream. array-stream)
@@ -97,12 +117,13 @@
 		       *date-formatter*
 		       (new-date-formatter))
 	time-stamp (:time data)
-	day (Integer/parseInt (.format #^SimpleDateFormat date-format time-stamp))]
-    (when (not (= @current-day day))
-      (load-day day))
-    (.writeInt data-out day)
+	day (Integer/parseInt (.format #^SimpleDateFormat date-format time-stamp))
+	the-day (get-day day)]
+    
+    (.writeInt data-out day) ;denna skall bort
     (.writeLong data-out (.getTime #^Date time-stamp))
     (.writeDouble data-out (:value data))
+    ;(println (index-for-name day (:host data)))
     (.writeInt data-out (index-for-name day (:host data)))
     (.writeInt data-out (index-for-name day (:category data)))
     (.writeInt data-out (index-for-name day (:counter data)))
@@ -137,10 +158,8 @@
 
 (defn from-bytes [bytes]
   (let [buffer (ByteBuffer/wrap bytes)
-	day (let [a-day (.getInt buffer)]
-	      (when (not (= @current-day a-day))
-		(load-day a-day))
-	      a-day)
+	day (.getInt buffer) ;denna skall bort
+	the-day (get-day day)
 	timestamp (Date. (.getLong buffer))
 	value (.getDouble buffer)
 	host (name-for-index day (.getInt buffer))
@@ -148,15 +167,38 @@
 	counter (name-for-index day (.getInt buffer))
 	instance (name-for-index day (.getInt buffer))
 	additional (int (.get buffer))
-	r (assoc {} :time timestamp :value value :host host :counter counter :category category :instance instance)]
+	r (assoc {} 
+	    :time timestamp 
+	    :value value 
+	    :host host 
+	    :counter counter 
+	    :category category 
+	    :instance instance)]
     (if (< 0 additional)
       (loop [i 0 v r]
 	(if (< i additional)
-	  (recur (inc i) (assoc v 
-			   (keyword (name-for-index day (.getInt buffer))) 
-			   (name-for-index day (.getInt buffer))))
+	  (recur (inc i) 
+		 (assoc v 
+		   (keyword (name-for-index day (.getInt buffer))) 
+		   (name-for-index day (.getInt buffer))))
 	  v))
       r)))
+
+
+(deftest test-index-for-name
+  (let [df (SimpleDateFormat. "yyyyMMdd HHmmss")]
+    (reset! lru [])
+    (reset! current-days {})
+    (binding [*date-formatter* (new-date-formatter)]
+      (let [data (assoc {} :time (.parse df "20071126 033332")  :value 23.0 :host "1" :counter "3" :category "2" :instance "4" :slangbella "454")
+	    data2 (assoc {} :time (.parse df "20071126 033332") :value 24.0 :host "two" :counter "four" :category "one" :instance "five")]
+	
+	(is (= 0 (index-for-name "Tokstolle" (:host data))))
+	(is (= 0 (index-for-name "Tokstolle" (:host data))))
+	(is (= 1 (index-for-name "Tokstolle" (:category data))))
+	(is (= 2 (index-for-name "Tokstolle" (:slangbella data))))))))
+
+
 
 (deftest testindices-with-persistence
  (let [tmp (make-temp-dir)]
@@ -165,7 +207,8 @@
       (binding [*date-formatter* (new-date-formatter)]
 	(let [data (assoc {} :time (.parse df "20071126 033332")  :value 23.0 :host "1" :counter "3" :category "2" :instance "4")
 	      another-data (assoc {} :time (.parse df "20071125 033332")  :value 2.0 :host "1" :counter "3" :category "2" :instance "4")
-	      some-more-data (assoc {} :time (.parse df "20071125 033332") :value 3.0 :host "1" :counter "33" :category "2" :instance "44")]
+	      some-more-data (assoc {} :time (.parse df "20071125 033332") :value 3.0 :host "1" :counter "33" :category "2" :instance "44")
+	      even-more-data (assoc {} :time (.parse df "20071125 033332") :value 3.0 :host "1" :counter "33" :category "2" :instance "44" :gurkmeja "meja")]
 
 	  (using-db tmp "test" [:date :olle :nisse]
 		    (using-dayindex-db @*db-env* "dayindex-test"
@@ -175,22 +218,51 @@
 					 (is (= 23.0 (:value (from-bytes bytes))))
 					 (let [another-bytes (to-bytes another-data)]
 					   (is (= 20071125 (day-of another-bytes)))
-					   (is (= another-data (from-bytes another-bytes)))
 					   (is (= 23.0 (:value (from-bytes bytes))))
 					   (is (= 2.0 (:value (from-bytes another-bytes))))
 					   (is (= 23.0 (:value (from-bytes bytes))))
+					   (is (= another-data (from-bytes another-bytes)))
 					   (let [some-more-bytes (to-bytes some-more-data)]
 					     (is (= 2.0 (:value (from-bytes another-bytes))))
 					     (is (= 3.0 (:value (from-bytes some-more-bytes))))
 					     (is (= 23.0 (:value (from-bytes bytes))))
-					   (is (= 2.0 (:value (from-bytes another-bytes))))
+					     (is (= 2.0 (:value (from-bytes another-bytes))))
 					     (is (= 3.0 (:value (from-bytes some-more-bytes))))
-					     
-					 ))))))))
+					     (is (= #{0 1 2 3 4 5} 
+						    (set (for [x [bytes another-bytes some-more-bytes] 
+							       y [host-index-of category-index-of counter-index-of instance-index-of]]
+							   (y x)))))
+					     (is (= #{0 1 2 3} 
+						    (set (for [x [another-bytes bytes] 
+							       y [host-index-of category-index-of counter-index-of instance-index-of]]
+							   (y x)))))
+					     (is (= some-more-data (from-bytes some-more-bytes)))
+					     (let [even-more-bytes (to-bytes even-more-data)]
+					       (is (= even-more-data (from-bytes even-more-bytes)))))))
+
+				       (is (= 6 (index-for-name 20071125 "gurkmeja")))
+				       (reset! current-days {})
+				       (reset! lru [])
+				       (let [some-more-bytes (to-bytes some-more-data)]
+					 (is (= 6 (index-for-name 20071125 "gurkmeja")))
+					 (is (every? 
+					      (reduce #(conj %1 (%2 some-more-bytes)) 
+						      #{} 
+						      [host-index-of category-index-of counter-index-of instance-index-of])
+					      #{4 5}))
+					 (is (= some-more-data (from-bytes some-more-bytes)))
+					 (let [bytes (to-bytes data)]
+					   (is (= 20071126 (day-of bytes)))
+					   (is (= data (from-bytes bytes)))
+					   (is (= 23.0 (:value (from-bytes bytes)))))
+				       
+					 ))))))
     (finally (rmdir-recursive tmp)))))
 
 
 (deftest testindices
+  (reset! current-days {})
+  (reset! lru [])
   (let [df (SimpleDateFormat. "yyyyMMdd HHmmss")]
     (binding [*date-formatter* (new-date-formatter)]
       (let [data (assoc {} :time (.parse df "20071126 033332")  :value 23.0 :host "1" :counter "3" :category "2" :instance "4")]
@@ -202,8 +274,20 @@
 	  (is (= 1 (category-index-of bytes)))
 	  (is (= 2 (counter-index-of bytes)))
 	  (is (= 3 (instance-index-of bytes)))
-	  (is (= data (from-bytes bytes)))))
-      (let [data (assoc {} :time (.parse df "20071126 033332")  :value 23.0 :host "1" :counter "3" :category "2" :instance "4" :7 "3")]
+	  (is (= data (from-bytes bytes)))
+	  (is (= (reduce #( conj %1 (name-for-index 20071126 %2)) [] (range 4) )
+		 ["1" "2" "3" "4"]))
+	  (is (= (reduce #( conj %1 (index-for-name 20071126 %2)) [] ["1" "2" "3" "4"] )
+		 (range 4)))
+	   ))
+      
 
+      (let [data (assoc {} :time (.parse df "20071127 033332")  :value 23.0 :host "1" :counter "3" :category "2" :instance "4" :oljemagnat "magnaten")]
 	(let [bytes (to-bytes data)]
-	  (is (= data (from-bytes bytes))))))))
+	  (is (= data (from-bytes bytes)))
+	  (is (= (reduce #( conj %1 (name-for-index 20071127 %2)) [] (range 6) )
+		 ["1" "2" "3" "4" "oljemagnat" "magnaten"]))
+	  (is (= (reduce #( conj %1 (index-for-name 20071126 %2)) [] ["1" "2" "3" "4" "oljemagnat" "magnaten"] )
+		 (range 6))))))))
+
+      
