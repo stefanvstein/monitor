6a89032954b023ceeca9d9583f2f1b33aa6e52f1
Stefan von Stein <stefan.vonstein@it-huset.se>
2010-10-14 15:59:17 +0200

    clean of logger

--- src/monitor/logger.clj
+++ src/monitor/logger.clj
@@ -41,41 +41,10 @@
   
 (defmacro using-logger
   [& form]
-  `(let [logger# (Logger/getLogger "monitor")
-	 handler# (let [ han# (ConsoleHandler.)
-			lastHead# (atom "")]
-			(. han# setFormatter 
-			   (proxy [Formatter] []
-			     (getHead [h#] "")
-			     (getTail [h#] "")
-			     (format [logRecord#]
-				     (let [df# (java.text.SimpleDateFormat. "yyyy-MM-dd HH:mm:ss")
-					   thisHead# 
-					   (with-out-str 
-					     (print (str 
-						     (.format df# (java.util.Date. (.getMillis logRecord#)))
-						     " " 
-						     (.getLoggerName logRecord#))))]
-				       (with-out-str
-					 (when-not (= @lastHead# thisHead#)
-					   (do 
-					     (println thisHead#)
-					     (swap! lastHead# (fn [_#] thisHead#))))
-					 (println (str "  " 
-						       (. logRecord# getLevel) 
-						       ": " 
-						       (.getMessage logRecord#)))
-					 (when-let [ex# (.getThrown logRecord#)]
-					   (print-cause-trace ex#)
-					   (swap! lastHead# (fn [_#] "")))))))) 
-			han#)]
-	  (. logger# addHandler handler#)
-	  (. logger# setLevel Level/FINEST)
-	  (. handler# setLevel Level/FINEST)
-	  (. logger# setUseParentHandlers false)
+  `(let [stop-fn# (use-logger)]
 	  (try
 	   (do ~@form)
-	  (finally (. logger# removeHandler handler#)))))
+	  (finally (stop-fn#)))))
 
 (deftest try-some-logging
     (using-logger
