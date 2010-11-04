879bc96c0af6c31fadf44a185a54d61b3b25e569
Stefan von Stein <stefan.vonstein@it-huset.se>
2010-11-04 10:34:00 +0100

    Added spec of client transfer port in in-env

--- src/monitor/monitor.clj
+++ src/monitor/monitor.clj
@@ -19,50 +19,55 @@
 (defn linux-proc [host port]
   (dosync (alter tasks-to-start conj #(process-remote-linux-proc host port terminating?))))
 
-(defn in-env [ live-minutes history-days history-directory client-port]
-  (when (not (System/getProperty "nogui"))
-    (shutdown-button "Monitor server"))
-  (shutdown-jmx "monitor.server")
-  				  
- 
-  
-  (using-live
-   (dosync (alter tasks-to-start conj
-		  (fn live-cleaner []
-		    (while (not (terminating?))
-		      (try (term-sleep 30)
-			   (catch InterruptedException _#))
-		      (when-not (terminating?)
-			(clean-live-data-older-than 
-			 (java.util.Date. (- (System/currentTimeMillis) 
-					     (* 1000 60 live-minutes)))))))))
-   (using-history history-directory
-		  (jmx-db-record-counter "monitor.server")
-		  
-		  (dosync
-		   (alter tasks-to-start conj
-			  (fn syncer []
-			    (while (not (terminating?))
-					(try (term-sleep (* 1 60))
-					     (catch InterruptedException _))
-					(sync-db))))
-					     
-		   (alter tasks-to-start conj
-				 (fn history-cleaner []
-				   (while (not (terminating?))
-				     (try (term-sleep (* 60 60 3 ))
-					  (catch InterruptedException _))
-				     (when-not (terminating?)
-				       (clean-stored-data-older-than 
-					(java.util.Date. (- (System/currentTimeMillis) 
-							    (* history-days 24 1000 60 60))))
-				       (compress-older-than
-					(java.util.Date.)
-					terminating?))))))
-		  
-		  (serve-clients (+ client-port 1) client-port terminating?
-				 (serve))))
-  (println "Done"))		    
+(defn in-env 
+  ([live-minutes history-days history-directory client-port client-transfer-port]
+     (when (not (System/getProperty "nogui"))
+       (shutdown-button "Monitor server"))
+     (shutdown-jmx "monitor.server")
+     
+     
+     
+     (using-live
+      (dosync (alter tasks-to-start conj
+		     (fn live-cleaner []
+		       (while (not (terminating?))
+			 (try (term-sleep 30)
+			      (catch InterruptedException _#))
+			 (when-not (terminating?)
+			   (clean-live-data-older-than 
+			    (java.util.Date. (- (System/currentTimeMillis) 
+						(* 1000 60 live-minutes)))))))))
+      (using-history history-directory
+		     (jmx-db-record-counter "monitor.server")
+		     
+		     (dosync
+		      (alter tasks-to-start conj
+			     (fn syncer []
+			       (while (not (terminating?))
+				 (try (term-sleep (* 1 60))
+				      (catch InterruptedException _))
+				 (sync-db))))
+		      
+		      (alter tasks-to-start conj
+			     (fn history-cleaner []
+			       (while (not (terminating?))
+				 (try (term-sleep (* 60 60 3 ))
+				      (catch InterruptedException _))
+				 (when-not (terminating?)
+				   (clean-stored-data-older-than 
+				    (java.util.Date. (- (System/currentTimeMillis) 
+							(* history-days 24 1000 60 60))))
+				   (compress-older-than
+				    (java.util.Date.)
+				    terminating?))))))
+		     
+		     (serve-clients client-transfer-port client-port terminating?
+				    (serve))))
+     (println "Done"))
+  ([live-minutes history-days history-directory client-port]
+     (in-env live-minutes history-days history-directory client-port 0))
+  ([history-days history-directory client-port]
+     (in-env 65 history-days history-directory client-port 0)))
 
 
 
