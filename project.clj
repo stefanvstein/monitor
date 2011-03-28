d63e06b2117586a88458a20618e099483ab38bfa
stefanvstein <stefan.vonstein@it-huset.se>
2011-03-28 11:43:36 +0200

    Updated to 0.0.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject Monitor "0.0.5"
+(defproject Monitor "0.0.6"
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
